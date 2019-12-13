e1cc3a19bf8f5c074f5f5db137e4ece9cda061f3
baiyang <625467820@qq.com>
2019-12-13 17:34:49 +0800

    首页banner

--- src/page/home/components/Banner.js
+++ src/page/home/components/Banner.js
@@ -1,23 +1,60 @@
 import React, {Component} from "react";
-import {BannerWrapper, Center} from '../style';
+import {BannerWrapper, Center, Focusinfo} from '../style';
 
-class Banner extends Component{
+class Banner extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            backgroundImage: this.getBanner()
+        };
+        this.changeBanner = this.changeBanner.bind(this);
+    }
+
     render() {
-        return(
+        return (
             <BannerWrapper>
-                <Center style={{backgroundImage:this.getBanner()}}>banner</Center>
+                <Center style={{backgroundImage: this.state.backgroundImage}}>
+                    <Focusinfo>
+                        <h1 className='glitch' data-text="Hi, Mashiro!">Hi, Mashiro!</h1>
+                        <div className='header-info'>
+                            <p> You got to put the past behind you before you can move on. </p>
+                            <div className='top-social_v2'>
+                                <li onClick={this.changeBanner}><img className='flipx' src={require('../../../statics/images/next-b.svg')} alt=""/>
+                                </li>
+                                <li><img src={require('../../../statics/images/github.png')} alt=""/></li>
+                                <li><img src={require('../../../statics/images/sina.png')} alt=""/></li>
+                                <li><img src={require('../../../statics/images/wangyiyun.png')} alt=""/></li>
+                                <li><img src={require('../../../statics/images/zhihu.png')} alt=""/></li>
+                                <li><img src={require('../../../statics/images/qq.png')} alt=""/></li>
+                                <li><img src={require('../../../statics/images/email.svg')} alt=""/></li>
+                                <li onClick={this.changeBanner}><img
+                                    src={require('../../../statics/images/next-b.svg')} alt=""/></li>
+                            </div>
+                        </div>
+                    </Focusinfo>
+                </Center>
             </BannerWrapper>
         )
     }
-    getBanner(){
-        const bannerList = [
-            {img:require('../../../statics/images/BG-001.jpg')},
-            {img:require('../../../statics/images/BG-002.jpg')}
-        ];
-        const num = this.getrand(0,bannerList.length-1);
-        return 'url('+bannerList[num].img+')'
+
+    changeBanner() {
+        this.setState(()=>{
+            return{
+                backgroundImage: this.getBanner()
+            }
+        })
     }
-    getrand(m,n){
+
+    getBanner() {
+        const bannerList = [
+            {img: require('../../../statics/images/BG-001.jpg')},
+            {img: require('../../../statics/images/BG-002.jpg')}
+        ];
+        const num = this.getrand(0, bannerList.length - 1);
+        return 'url(' + bannerList[num].img + ')'
+    }
+
+    getrand(m, n) {
         return Math.floor(Math.random() * (n - m + 1)) + m;
     }
 }
--- src/page/home/style.js
+++ src/page/home/style.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import grid from '../../statics/images/grid.png';
+
 export const BannerWrapper = styled.div`
 	position: relative;
     overflow: hidden;
@@ -28,4 +29,215 @@ export const Center = styled.div`
     background-attachment: fixed;
     background-size: cover;
     z-index: -1;
+`;
+
+export const Focusinfo = styled.div`
+    position: relative;
+    max-width: 800px;
+    padding: 0 10px;
+    top: 49.3%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    -webkit-transform: translate(-50%,-50%);
+    text-align: center;
+    z-index: 99;
+    -webkit-transition: .4s ease all;
+    -moz-transition: .4s ease all;
+    -o-transition: .4s ease all;
+    transition: .4s ease all;  
+    .glitch{
+        font-family: 'Ubuntu',sans-serif;
+        position: relative;
+        color: #fff;
+        mix-blend-mode: lighten;
+        margin: auto;
+        font-size: 80px;
+        text-transform: uppercase;
+        font-weight: bold;
+    }
+    .glitch:before, .glitch:after {
+        content: attr(data-text);
+        position: absolute;
+        top: 0;
+        width: 100%;
+        background: rgba(0,0,0,0);
+        clip: rect(0,0,0,0);
+    }
+    .glitch:before {
+        left: -1px;
+        text-shadow: 1px 0 #ff3f1a;
+    }
+    .glitch:after {
+        left: 1px;
+        text-shadow: -1px 0 #00a7e0;
+    }
+    .glitch:hover:before {
+        text-shadow: 4px 0 #ff3f1a;
+        animation: glitch-loop-1 .8s infinite ease-in-out alternate-reverse;
+    }
+    .glitch:hover:after {
+        text-shadow: -5px 0 #00a7e0;
+        animation: glitch-loop-2 .8s infinite ease-in-out alternate-reverse;
+    }
+    @-webkit-keyframes glitch-loop-1 {
+        0% {
+            clip: rect(36px,9999px,9px,0)
+        }
+    
+        25% {
+            clip: rect(25px,9999px,99px,0)
+        }
+    
+        50% {
+            clip: rect(50px,9999px,102px,0)
+        }
+    
+        75% {
+            clip: rect(30px,9999px,92px,0)
+        }
+    
+        100% {
+            clip: rect(91px,9999px,98px,0)
+        }
+    }
+    
+    @keyframes glitch-loop-1 {
+        0% {
+            clip: rect(36px,9999px,9px,0)
+        }
+    
+        25% {
+            clip: rect(25px,9999px,99px,0)
+        }
+    
+        50% {
+            clip: rect(50px,9999px,102px,0)
+        }
+    
+        75% {
+            clip: rect(30px,9999px,92px,0)
+        }
+    
+        100% {
+            clip: rect(91px,9999px,98px,0)
+        }
+    }
+    
+    @-webkit-keyframes glitch-loop-2 {
+        0% {
+            top: -1px;
+            left: 1px;
+            clip: rect(65px,9999px,119px,0)
+        }
+    
+        25% {
+            top: -6px;
+            left: 4px;
+            clip: rect(79px,9999px,19px,0)
+        }
+    
+        50% {
+            top: -3px;
+            left: 2px;
+            clip: rect(68px,9999px,11px,0)
+        }
+    
+        75% {
+            top: 0;
+            left: -4px;
+            clip: rect(95px,9999px,53px,0)
+        }
+    
+        100% {
+            top: -1px;
+            left: -1px;
+            clip: rect(31px,9999px,149px,0)
+        }
+    }
+    
+    @keyframes glitch-loop-2 {
+        0% {
+            top: -1px;
+            left: 1px;
+            clip: rect(65px,9999px,119px,0)
+        }
+    
+        25% {
+            top: -6px;
+            left: 4px;
+            clip: rect(79px,9999px,19px,0)
+        }
+    
+        50% {
+            top: -3px;
+            left: 2px;
+            clip: rect(68px,9999px,11px,0)
+        }
+    
+        75% {
+            top: 0;
+            left: -4px;
+            clip: rect(95px,9999px,53px,0)
+        }
+    
+        100% {
+            top: -1px;
+            left: -1px;
+            clip: rect(31px,9999px,149px,0)
+        }
+    }
+    .header-info {
+        position: relative;
+        width: 63%;
+        margin: auto;
+        font-size: 16px;
+        color: #eaeadf;
+        background: rgba(0,0,0,.5);
+        padding: 15px;
+        margin-top: 22px;
+        letter-spacing: 0;
+        line-height: 30px;
+        border-radius: 10px;
+        box-sizing: initial;
+        white-space: nowrap;
+    }
+    .header-info:before {
+        content: "";
+        position: absolute;
+        top: -30px;
+        left: 15%;
+        border-width: 15px;
+        border-style: solid;
+        border-color: transparent transparent rgba(0,0,0,.5) transparent;
+    }
+    .header-info p {
+        margin: 0;
+        font-family: 'Ubuntu',sans-serif;
+        font-weight: 700;
+    }
+    .top-social_v2 {
+        height: 35px;
+        margin-bottom:-10px;
+        list-style: none;
+        display: inline-block;
+    }
+    .top-social_v2 li {
+        height: 35px;
+        float: left;
+        margin:0 6px;
+        cursor: pointer;
+    }
+    .top-social_v2 img {
+        height: 35px;
+        width: 35px;
+        padding: 6px;
+        background: 0 0;
+    }
+    .flipx {
+        -moz-transform: scaleX(-1);
+        -webkit-transform: scaleX(-1);
+        -o-transform: scaleX(-1);
+        transform: scaleX(-1);
+        filter: FlipH;
+    }
 `;
