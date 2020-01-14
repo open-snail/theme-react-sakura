c0e4366655c2fa40d0f4939e85f457b2ab475370
baiyang <625467820@qq.com>
2020-01-14 15:56:10 +0800

    更新banner，修改默认看板娘模型

--- public/assets/autoload.js
+++ public/assets/autoload.js
@@ -6,7 +6,7 @@ try {
     /* 可直接修改部分参数 */
     live2d_settings['hitokotoAPI'] = 'hitokoto.cn';  // 一言 API
     live2d_settings['modelId'] = 6;                  // 默认模型 ID
-    live2d_settings['modelTexturesId'] = 7;         // 默认材质 ID
+    live2d_settings['modelTexturesId'] = 5;         // 默认材质 ID
     live2d_settings['modelStorage'] = false;         // 不储存模型 ID
     live2d_settings['waifuEdgeSide'] = 'right:30';
     live2d_settings['homePageUrl'] = '/';
